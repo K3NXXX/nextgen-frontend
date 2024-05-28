10cc400dcbaf1f60f2e0d1bdcb81cf4631c47342
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-05-28 18:13:49 +0300

    ui: did sticky header and aside bar in dashboard page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,17 @@
 import { DashboardAside } from '@/components/dashboard/dashboard-aside/DashboardAside'
 import { DashboardHeader } from '@/components/dashboard/dashboard-header/DashboardHeader'
+import { DashboardSetting } from '@/components/dashboard/dashboard-settings/DashboardSetting'
 import type { PropsWithChildren } from 'react'
 import styles from './Dashboard.module.scss'
-import { DashboardSetting } from '@/components/dashboard/dashboard-settings/DashboardSetting'
 
 export default function Layout({ children }: PropsWithChildren<unknown>) {
     return (
         <div className={styles.root}>
             <div className={styles.container}>
-                <DashboardAside />
+                    <DashboardAside />
                 <main className={styles.main}>
                     <DashboardHeader />
-                    <DashboardSetting/>
+                    <DashboardSetting />
                     {children}
                 </main>
             </div>
--- src/components/dashboard/dashboard-header/DashboardHeader.tsx
+++ src/components/dashboard/dashboard-header/DashboardHeader.tsx
@@ -1,24 +1,46 @@
 'use client'
 
 import { Heading } from '@/components/ui/dashboard/heading/Heading'
+import { capitalizeFirstLetter } from '@/utils/capitalize-first-letter'
 import { Bell, Search, Settings, User } from 'lucide-react'
 import { usePathname } from 'next/navigation'
-import { AiFillHome } from "react-icons/ai";
+import { AiFillHome } from 'react-icons/ai'
 import styles from './DashboardHeader.module.scss'
-import { capitalizeFirstLetter } from '@/utils/capitalize-first-letter'
+import { useEffect, useRef } from 'react'
 
 export function DashboardHeader() {
-    const pathname = usePathname();
-    const capitalizedPathname = capitalizeFirstLetter(pathname);
+    const pathname = usePathname()
+    const capitalizedPathname = capitalizeFirstLetter(pathname)
+    const headerRef = useRef<HTMLHeadElement>(null);
+
+    useEffect(() => {
+        const header = headerRef.current;
+
+        const handleScroll = () => {
+            if (header) {
+                const isAtTop = window.scrollY === 0; 
+                const isSticky = header.getBoundingClientRect().top <= 20 && !isAtTop; // 
+                if (isSticky) {
+                    header.classList.add(styles.sticky);
+                } else {
+                    header.classList.remove(styles.sticky);
+                }
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <header className={styles.root}>
+        <header  ref={headerRef} className={styles.root}>
             <div className={styles.left__part}>
                 <div className={styles.pages}>
                     <div className={styles.pages__top}>
                         <p>
-                            <AiFillHome size={15}/>
+                            <AiFillHome size={15} />
                             <span>/</span>
-                             <span> {capitalizedPathname}</span>
+                            <span> {capitalizedPathname}</span>
                         </p>
                     </div>
                     <Heading title="Dashboard" />
@@ -26,13 +48,17 @@ export function DashboardHeader() {
             </div>
             <div className={styles.right__part}>
                 <div className={styles.input__wrapper}>
-                    <Search className={styles.search} color='#718096' width={20} />
+                    <Search
+                        className={styles.search}
+                        color="#718096"
+                        width={20}
+                    />
                     <input placeholder="Type here..." type="text" />
                 </div>
                 <div className={styles.icons}>
-                    <User color='#718096' />
-					<Settings color='#718096' />
-					<Bell color='#718096' />
+                    <User color="#718096" />
+                    <Settings color="#718096" />
+                    <Bell color="#718096" />
                 </div>
             </div>
         </header>
