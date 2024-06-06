4563e9eb1be66c2e6d3d6e59ab407b10873b5a5e
Volodya Andrushevskyi <xxvolodyax7@gmail.com>
2024-06-06 22:05:38 +0300

    ui: added profile info row on profile page

--- src/app/dashboard/profile/Profile.tsx
+++ src/app/dashboard/profile/Profile.tsx
@@ -1,11 +1,13 @@
 
 import { Navigation } from '@/components/dashboard/profile/navigation/Navigation'
 import styles from "./Profile.module.scss"
+import { ProfileInfo } from '@/components/dashboard/profile/profile-info/ProfileInfo'
 
 export default async function Profile() {
 	return (
 		<div className={styles.root}>
 			<Navigation/>
+			<ProfileInfo/>
 		</div>
 	)
 
---
+++ src/components/dashboard/profile/profile-info/ProfileInfo.tsx
@@ -0,0 +1,58 @@
+import info from '@/../public/dashboard-page/profile/info.png'
+import { MoveRight } from 'lucide-react'
+import Image from 'next/image'
+import styles from './ProfileInfo.module.scss'
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { IoLogoInstagram } from "react-icons/io5";
+
+export function ProfileInfo() {
+    return (
+        <div className={styles.root}>
+            <div className={styles.welcomeBack}>
+                <div className={styles.top}>
+                    <p>Welcome back!</p>
+                    <span>Nice to see you, Mark Johnson!</span>
+                </div>
+                <div className={styles.bottom}>
+                    <p>Tap to record</p>
+                    <MoveRight color="white" size={20} />
+                </div>
+            </div>
+            <div className={styles.info}>
+                <Image src={info} alt="info" />
+            </div>
+            <div className={styles.profileInfo}>
+                <h6>Profile Informations</h6>
+                <p className={styles.shortInfo}>
+                    Hi, I’m Mark Johnson, Decisions: If you can’t decide, the
+                    answer is no. If two equally difficult paths, choose the one
+                    more painful in the short term (pain avoidance is creating
+                    an illusion of equality).
+                </p>
+                <div className={styles.content}>
+                    <div>
+                        <p>Full Name: </p>
+                        <span>Mark Johnson</span>
+                    </div>
+                    <div>
+                        <p>Mobile: </p>
+                        <span>(44) 123 1234 123</span>
+                    </div>
+					<div>
+                        <p>Location: </p>
+                        <span>United States3</span>
+                    </div>
+					<div>
+                        <p>Social Media: </p>
+                        <div className={styles.socials}>
+							<FaFacebook color='white' size={17}/>
+							<FaTwitter  color='white' size={17}/>
+							<IoLogoInstagram  color='white' size={17} />
+						</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
